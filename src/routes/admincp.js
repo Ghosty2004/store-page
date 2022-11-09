1f3956eefecd8213e1c03f368673e4f8437caa33
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-09 18:46:09 +0200

    Started

--- src/routes/admincp.js
+++ src/routes/admincp.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getUserBySessionId, renderPage } from "../functions/index.js";
 
 const router = Router();
 
-router.get("/", (request, response) => {
-    
+router.get("/", async (request, response) => {
+    const user = await getUserBySessionId(request.cookies.sessionId);
+    if(!user || !user.isAdmin) return response.redirect("/");
+    renderPage(request, response, "admincp");
 });
 
 export default router;
