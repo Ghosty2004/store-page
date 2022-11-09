a213ea96eaa40b764bad29a3c2542b3e467c9b0c
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-09 18:47:53 +0200

    Added `./constants` in `/utils`

---
+++ src/utils/constants/index.js
@@ -0,0 +1,8 @@
+export const categories = {
+    "ALL": 0,
+    "PC": 1,
+    "LAPTOP": 2,
+    "TV": 3,
+    "GAMING": 4,
+    "ELECTRONICS": 5
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export * from "./db/index.js";
 export * from "./schema/index.js";
 export * from "./collection/index.js";
+export * from "./constants/index.js";
