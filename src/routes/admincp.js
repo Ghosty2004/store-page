e1a0b685d7e03082b9db21f47f82110173a439b4
Ghosty2004 <fejerszilveszter@gmail.com>
2022-11-10 21:00:13 +0200

    Added route `/remove-item` with `DELETE` method

--- src/routes/admincp.js
+++ src/routes/admincp.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { isValidObjectId } from "mongoose";
 import { getUserBySessionId, renderPage } from "../functions/index.js";
 import { categories, itemsCollection } from "../utils/index.js";
 
@@ -18,18 +19,38 @@ router.put("/add-item", async (request, response) => {
     const user = await getUserBySessionId(request.cookies.sessionId);
     if(!user) return response.json({ error: true, message: "You are not logged in!" });
     if(!user.isAdmin) return response.json({ error: true, message: "You are not an admin!" });
-    const { itemName, itemDescription, itemCategory, itemPrice, itemStock } = request.body;
+    const { itemName, itemDescription, itemCategory, itemPrice, itemStock, itemImages } = request.body;
     new itemsCollection({ 
         name: itemName,
         description: itemDescription,
-        category: itemCategory,
+        categoryType: itemCategory,
         price: itemPrice,
-        stock: itemStock
+        stock: itemStock,
+        images: itemImages
     }).save().then(() => {
         response.json({ error: false, message: "Item added successfully!" });
     }).catch(() => {
         response.json({ error: true, message: "An error occurred while adding the item!" });
     });
+});
+
+router.delete("/remove-item", async (request, response) => {
+    const user = await getUserBySessionId(request.cookies.sessionId);
+    if(!user) return response.json({ error: true, message: "You are not logged in!" });
+    if(!user.isAdmin) return response.json({ error: true, message: "You are not an admin!" });
+    const { itemId } = request.body;
+    try {
+        if(!isValidObjectId(itemId)) return response.json({ error: true, message: "Invalid item ID format!" });
+        if(await itemsCollection.exists({ _id: itemId })) {
+            await itemsCollection.deleteOne({ _id: itemId });
+            response.json({ error: false, message: "Item removed successfully!" });
+        } else {
+            response.json({ error: true, message: "Item not found!" });
+        }
+    } catch(e) {
+        console.log(e.stack);
+        response.json({ error: true, message: "An error occurred while removing the item!" });
+    }
 });
 
 export default router;
